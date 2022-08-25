868e1d8ea437f5a15b0c151fea83d36b37a07f64
SiyuanWu-Sabrina <saber_wu@126.com>
2022-08-25 20:13:37 +0800

    remove unused import

--- uniform_testing.py
+++ uniform_testing.py
@@ -1,25 +1,9 @@
 from __future__ import print_function
 from __future__ import division
 
-import os
-import sys
-import argparse
-import numpy as np
-from PIL import Image
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-import torchvision
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torchvision import datasets, transforms
-
-from utils.imagenet_dataset import ImageNetDataset
-from torch.utils.data import DataLoader
-from torchvision.transforms import ToPILImage
 import models.inception_v3_imagenet as inception_v3
-import models.generators as generators
 import models.resnet_cifar10 as resnet
 from argparse import Namespace
 
