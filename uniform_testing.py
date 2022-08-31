49e87c3f298c37ab112e55723f9e2c3149dc7cb0
SiyuanWu-Sabrina <saber_wu@126.com>
2022-08-31 21:20:00 +0800

    #2 overall test version for gf and sapf

--- uniform_testing.py
+++ uniform_testing.py
@@ -107,20 +107,22 @@ def configuration(attack_algorithm, dataset_setting, targeted=False, batch_size=
 
 
 def overall():
-    attack_list = ['B3D_b', 'greedyfool_w', 'cornersearch_b', 'PGD_attack_w', 'homotopy_w', 'perturbation_b']
+    attack_list = ['greedyfool_w', 'greedyfool_b', 'perturbation_b']
     data_list = ['Cifar10', 'ImageNet']
+
     for data in data_list:
         for attack in attack_list:
-            print(f"==========Testing on: {data}, attack type: {attack}==========")
-            config = configuration(attack, data)
-            attack_algorithm = Attack(attack)
-            netT = target_net_factory(config.target_model)
-            test_attack_success_rate(config, netT, attack_algorithm.attack)
-            print(f"==========Test on: {data}, attack type: {attack} succeeded.==========")
+            for target in [True, False]:
+                print(f"==========Testing on: {data}, attack type: {attack}==========")
+                config = configuration(attack, data, batch_size=10, targeted=target)
+                attack_algorithm = Attack(attack)
+                netT = target_net_factory(config.target_model)
+                test_attack_success_rate(config, netT, attack_algorithm.attack)
+                print(f"==========Test on: {data}, attack type: {attack} succeeded.==========")
 
 
 def test():
-    attack_algorithm = 'perturbation_b'
+    attack_algorithm = 'greedyfool_w'
     # dataset_setting = 'ImageNet'
     dataset_setting = 'Cifar10'
 
@@ -136,5 +138,5 @@ def test():
 
 
 if __name__ == '__main__':
-    # overall()
-    test()
+    overall()
+    # test()
